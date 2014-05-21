7c632e4d09e4a9b83d8e3207842d8d6ef330d105
Uwe Kaminski <jukey@ju-key.de>
2014-05-21 21:26:31 +0200

    now the backup files put into a separate backup dir

--- custom-search-engines/install-custom-search-plugins.sh
+++ custom-search-engines/install-custom-search-plugins.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Make sure to run this script from the directory containing 
+this file
+
 # create a backup of existing files
 
-cp /usr/share/jolla-settings/pages/browser/browser.qml .
-cp /home/nemo/.mozilla/mozembed/searchplugins/* .
+mkdir ./backup
+cp /usr/share/jolla-settings/pages/browser/browser.qml ./backup
+cp /home/nemo/.mozilla/mozembed/searchplugins/* ./backup
 
 # copy custom search plugin files
 
@@ -10,5 +14,6 @@ cp ./*.xml /home/nemo/.mozilla/mozembed/searchplugins/
 
 # copy modified browser.qml
 
+devel-su
 cp ./browser.qml /usr/share/jolla-settings/pages/browser/
-
+exit
